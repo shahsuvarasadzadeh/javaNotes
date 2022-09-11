489c19c8666c6b52253d3a0daefadceb427e26e5
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2022-09-11 13:45:29 +0400

    update1

---
+++ src/variables/InstanceAndClassVariables.java
@@ -0,0 +1,30 @@
+package variables;
+
+public class InstanceAndClassVariables {
+    /*
+    Instance dəyişənlər həm də “fields” adlandırılır. Instance və class dəyişənlər lokal dəyişənlərdən fərqli
+olaraq classın daxilində, amma metodun xaricində elan edilir. Lokal dəyişənlərdən digər
+fərqləri isə odur ki, bu növ dəyişənlər istifadə edilmədən öncə dəyər mənimsədilməsi
+(initialize) tələb etmir. Instance və class dəyişənlər artıq elan olunan vaxtı onlara susmaya
+görə (default) dəyər mənimsədilir. Susmaya görə dəyərlərlə aşağıdakı cədvəldən tanış ola
+bilərsiniz:
+boolean - false
+byte, short, int, long -0
+float, double - 0.0
+char  - '\u0000'
+Bütün obyekt tipləri üçün - null
+
+Class dəyişənləri ilə instance dəyişənlərinin fərqi isə odur ki, class dəyişənlərin önündə static
+açar sözü yazılır, buna görə də bu dəyişənlər həm də static dəyişənlər adlandırılır. Static
+dəyişənlər classın instansını yaratmadan birbaşa class adının özü ilə müraciət etməklə digər
+classlarda çağırıla bilər.
+
+Variable Scope:
+
+Dəyişənlərin təsir dairəsi (variable scope) bu formadadır:
+• Lokal dəyişənlər – elan olunduğu sətirdən daxilində olduğu kod bloku bitənədək;
+• Instance dəyişənlər – elan olunduğu vaxtdan GC tərəfindən silinənədək;
+• Class dəyişənlər – elan olunduğu vaxtdan proqram sonlananadək.
+*/
+
+}
--- src/variables/LocalVaruables.java
+++ src/variables/LocalVaruables.java
@@ -2,4 +2,53 @@ package variables;
 
 public class LocalVaruables {
 
-}
+    /*Lokal dəyişənlər metod daxilində yaradılmış dəyişənlərdir. Lokal dəyişənlərin varsayılan
+            (default) dəyəri olmur, ona görə də onları istifadə etməzdən əvvəl mütləq dəyər
+    mənimsədilməlidir.
+*/
+
+    public void testLocalInitialization() {
+        int count = 5;
+        int number;
+        if (count > 5) number = 1;
+        else if (count <= 5) number = 2;
+        System.out.println(number); // doesn`t compile
+    }
+
+        // number dəyişəninə dəyər mənimsədilməmiş istifadə edildiyinə görə kompayl xətası verir.
+
+/*        number dəyişəninə dəyər mənimsədilməmiş istifadə edildiyinə görə kompayl xətası verir. Biz
+        fikirləşə bilərik ki, axı if şərtinin içərisində dəyər mənimsədilib, count ya 5-dən böyükdür, ya
+        bərabərdir, ya da kiçikdir. Axı istənilən halda bu iki şərtdən biri ödənəcək. Bəs onda niyə
+        kompilyator number`i dəyəri mənimsədilməmiş kimi görür?! Çünki kompayl zamanı (compile
+        time) kompilyator bunu təyin edə bilmir, ancaq icra vaxtı (runtime) bilir. Amma əgər biz
+        count dəyişənini final elan etsək o zaman kod kompayl olunacaq. Və yaxud da sona else
+        ifadəsi əlavə etsək, kompayl olunacaq. Çünki if-else strukturunda dəyər mənimsədildikdə
+        kompilyator başa düşür ki, heç bir şərtdən asılı olmayaraq istənilən halda if şərti ödənməsə,
+else blokuna müraciət olunacaq və dəyər mənimsədiləcək.*/
+
+        public void testLocalInitialization1() {
+            int count = 5;
+            int number;
+            if (count > 5) number = 1;
+            else if (count <= 5) number = 2;
+            else number = 3;
+            System.out.println(number); // does compile
+        }
+
+        /*
+        * Dəyişən final elan edildikdə isə kompilyator başa düşür ki, həmin dəyişənin dəyəri heç vaxt
+dəyişə bilməz və ona görə də kompilyator həmin dəyişənin dəyərini birbaşa şərtə qoyub
+yoxlayır.
+*/
+        public void testLocalInitialization2() {
+            final int count = 5;
+            int number ;
+            if (count > 8) number = 1;
+            else if (count <= 4) number = 2;
+            else number =3;
+                System.out.println(number);
+        }
+
+
+    }
